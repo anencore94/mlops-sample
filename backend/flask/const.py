b29277e0fbf123b60d28fad77b3e647df278560b
jaeyeon_kim <jaeyeon_kim@tmax.co.kr>
2020-06-30 17:32:20 +0900

    [flask] const 주석 추가

--- backend/flask/const.py
+++ backend/flask/const.py
@@ -8,8 +8,8 @@ FLASK_PORT = "3307"
 INPUT_MOUNT_PATH = '/input'
 OUTPUT_MOUNT_PATH = '/output'
 
-INPUT_HOST_PATH = '/hosthome/kjy/input/'
-OUTPUT_HOST_PATH = '/mnt/sda1/data/'
+INPUT_HOST_PATH = '/hosthome/kjy/input/'  # Dataset 이 준비되어 있는 host 의 경로
+OUTPUT_HOST_PATH = '/mnt/sda1/data/'  # 학습 결과를 저장할 host 의 경로
 
 # model config
 AE_MODEL_NAME = 'ae'
