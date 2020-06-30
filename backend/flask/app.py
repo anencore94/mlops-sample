643575967389c0fa69ace69cadc9e549bb887aeb
jaeyeon_kim <jaeyeon_kim@tmax.co.kr>
2020-06-30 17:24:34 +0900

    [flask] job spec 생성 로직을 k8s_util 모듈로 분리
    - job spec 생성 로직 분리
    - request parsing exception 추가

--- backend/flask/app.py
+++ backend/flask/app.py
@@ -7,9 +7,11 @@ import json
 from flask import Flask, Response, request
 from kubernetes import client, config
 from kubernetes.client.rest import ApiException
+from werkzeug.exceptions import BadRequestKeyError
 
 import const
-import dbutil
+import db_util
+import k8s_util
 
 app = Flask(__name__)
 
@@ -23,7 +25,7 @@ def get_model_info():
   :rtype: Response
 """
   # TODO try ~ exception ~ finally 로 변경
-  sql_output = dbutil.select_model_info()
+  sql_output = db_util.select_model_info()
 
   if len(sql_output) == 0:
     return {'StatusCode': '400', 'Message': 'There isn\'t any  model info'}
@@ -41,87 +43,45 @@ def create_ae_job():
   """
 autoencoer 학습을 진행하는 k8s job 을 하나 생성합니다.
 
-:param: learning_rate, epoch, experiment_id, output_mount_path
+:param: learning_rate, epoch, experiment_id
 :return: 생성 여부와 HTTP_STATUS 를 담은 Response
 :rtype: Response
 """
 
-  # request body 에서 필요한 정보 파싱
+  # STEP 1) request body 에서 필요한 정보 파싱
   # TODO UI 에서 보낼 때, json 인지 form 인지에 따라 추후 변경 필요할 수도 있음
-  # TODO parsing 못 할 경우 exception 추가
-  req_lr = request.form['learning_rate']
-  req_epoch = request.form['epoch']
-  req_ex_id = request.form['experiment_id']
+  try:
+    req_lr = request.form['learning_rate']
+    req_epoch = request.form['epoch']
+    req_ex_id = request.form['experiment_id']
+  except BadRequestKeyError as e:
+    print(e)
+    return Response("Wrong Input. Please check your request body", status=400)
 
+  # STEP 2) DB 에 experiment 정보 저장
+  # DB 에서 data file path 조회
+
+  # STEP 3) k8s job 생성 요청
   # pod 에서 client 사용하려면 sa 사용해서 config load 해야 함
   config.load_incluster_config()
-
-  # TODO job spec 만드는 것 k8s_util.py 분리해서 함수화
+  # client 만들기
   batch_api_client = client.BatchV1Api()
-  job = client.V1Job()
-
-  # label
-  label = dict()
-  label['mlmodel'] = 'ae'
-  job.metadata = client.V1ObjectMeta(name="ae",  # 이름 겹치지 않게 generator 사용
-                                     labels=label)
-
-  # volumes
-  in_hostpath = client.V1HostPathVolumeSource(path=const.INPUT_HOST_PATH,
-                                              type='Directory')
-  in_volume = client.V1Volume(name='in-storage', host_path=in_hostpath)
-  out_hostpath = client.V1HostPathVolumeSource(path=const.OUTPUT_HOST_PATH,
-                                               type='Directory')
-  out_volume = client.V1Volume(name='out-storage', host_path=out_hostpath)
-
-  # containerPort
-  container_port = client.V1ContainerPort(container_port=int(const.FLASK_PORT))
-
-  # volumeMount
-  in_volume_mount = client.V1VolumeMount(mount_path=const.INPUT_MOUNT_PATH,
-                                         name='in-storage')
-  out_volume_mount = client.V1VolumeMount(mount_path=const.OUTPUT_MOUNT_PATH,
-                                          name='out-storage')
-
-  # envs
-  env_lr = client.V1EnvVar(name='LEARNING_RATE', value=req_lr)
-  env_epoch = client.V1EnvVar(name='EPOCH', value=req_epoch)
-  env_ex_id = client.V1EnvVar(name='EXPERIMENT_ID', value=req_ex_id)
-  env_output_path = client.V1EnvVar(name='OUTPUT_MOUNT_POINT',
-                                    value=const.OUTPUT_MOUNT_PATH)
-  envs = [env_lr, env_epoch, env_ex_id, env_output_path]
-
-  # container
-  container = client.V1Container(name=const.AE_MODEL_NAME,
-                                 image=const.AE_MODEL_IMAGE_URL,
-                                 # TODO image tag 는 parameter 로 제공, DB 관리
-                                 image_pull_policy='Always',
-                                 ports=[container_port],
-                                 volume_mounts=[in_volume_mount,
-                                                out_volume_mount],
-                                 env=envs)
-
-  # pod spec
-  pod_spec = client.V1PodSpec(service_account_name=const.SERVICE_ACCOUNT_NAME,
-                              restart_policy='OnFailure',
-                              containers=[container],
-                              volumes=[in_volume, out_volume])
-  template = client.V1PodTemplateSpec(spec=pod_spec)
-
-  # job spec
-  spec = client.V1JobSpec(template=template, backoff_limit=3)
-  job.spec = spec
-
   try:
     batch_api_client.create_namespaced_job(namespace='default',
-                                           body=job)  # sync call
+                                           body=
+                                           k8s_util.make_job_spec(
+                                             lr=req_lr, epoch=req_epoch,
+                                             ex_id=req_ex_id))  # sync call
+    # 여기서 timeout 걸릴 경우 고려 필요
   except ApiException as e:
     print(e)
     return Response("Job Creation Failed", status=400)
 
-  # TODO Job 이 completed 될 때까지 wait
-  print('Job created')
+  # STEP 4) k8s job 이 Completed 될 때까지 wait
 
+  # STEP 5) DB 에 output_file_path 저장
+
+  # STEP 6) 성공/실패 여부 반환
   return Response("Job Created", status=200)
 
 
---
+++ backend/flask/k8s_util.py
@@ -0,0 +1,69 @@
+"""
+Util class for k8s access
+"""
+from kubernetes import client
+
+import const
+
+
+def make_job_spec(**kwargs):
+  """
+k8s job spec 을 생성합니다.
+
+:param: learning_rate, epoch, experiment_id
+:return: V1Job
+:rtype: V1Job
+"""
+  job = client.V1Job()
+  # label
+  label = dict()
+  label['mlmodel'] = 'ae'
+  job.metadata = client.V1ObjectMeta(name="ae",  # 이름 겹치지 않게 generator 사용
+                                     labels=label)
+
+  # volumes
+  in_hostpath = client.V1HostPathVolumeSource(path=const.INPUT_HOST_PATH,
+                                              type='Directory')
+  in_volume = client.V1Volume(name='in-storage', host_path=in_hostpath)
+  out_hostpath = client.V1HostPathVolumeSource(path=const.OUTPUT_HOST_PATH,
+                                               type='Directory')
+  out_volume = client.V1Volume(name='out-storage', host_path=out_hostpath)
+
+  # containerPort
+  container_port = client.V1ContainerPort(container_port=int(const.FLASK_PORT))
+
+  # volumeMount
+  in_volume_mount = client.V1VolumeMount(mount_path=const.INPUT_MOUNT_PATH,
+                                         name='in-storage')
+  out_volume_mount = client.V1VolumeMount(mount_path=const.OUTPUT_MOUNT_PATH,
+                                          name='out-storage')
+
+  # envs
+  env_lr = client.V1EnvVar(name='LEARNING_RATE', value=kwargs.get('lr'))
+  env_epoch = client.V1EnvVar(name='EPOCH', value=kwargs.get('epoch'))
+  env_ex_id = client.V1EnvVar(name='EXPERIMENT_ID', value=kwargs.get('ex_id'))
+  env_output_path = client.V1EnvVar(name='OUTPUT_MOUNT_POINT',
+                                    value=const.OUTPUT_MOUNT_PATH)
+  envs = [env_lr, env_epoch, env_ex_id, env_output_path]
+
+  # container
+  container = client.V1Container(name=const.AE_MODEL_NAME,
+                                 image=const.AE_MODEL_IMAGE_URL,
+                                 # TODO image tag 는 parameter 로 제공, DB 관리
+                                 image_pull_policy='Always',
+                                 ports=[container_port],
+                                 volume_mounts=[in_volume_mount,
+                                                out_volume_mount],
+                                 env=envs)
+
+  # pod spec
+  pod_spec = client.V1PodSpec(service_account_name=const.SERVICE_ACCOUNT_NAME,
+                              restart_policy='OnFailure',
+                              containers=[container],
+                              volumes=[in_volume, out_volume])
+  template = client.V1PodTemplateSpec(spec=pod_spec)
+
+  # job spec
+  spec = client.V1JobSpec(template=template, backoff_limit=3)
+  job.spec = spec
+  return job
